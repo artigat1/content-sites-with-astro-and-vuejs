e7d4181cc50eb7178d9a603022e230ac42710df0
Steve Doggett <steve@orbitalwitness.com>
2023-10-28 10:17:15 +0100

    Animate the astronaut to follow the mouse

---
+++ src/components/Astronaut.vue
@@ -0,0 +1,36 @@
+﻿<template>
+    <span id="rotateMe"
+          ref="el">🧑‍🚀</span>
+</template>
+
+<script lang="ts" setup>
+    import {useMouse} from '@vueuse/core'
+    import {computed, ref} from 'vue';
+    
+    const {x, y} = useMouse();
+    const el = ref<HTMLElement>();
+    const cssRotate = computed(() => {
+        if (!el.value) return '';
+        
+        const rect = el.value.getBoundingClientRect();
+        const centerX = rect.left + rect.width / 2;
+        const centerY = rect.top + rect.height / 2;
+    
+        const deltaX = x.value - centerX;
+        const deltaY = y.value - centerY;
+    
+        const rad = Math.atan2(deltaY, deltaX);
+        const deg = rad * (180 / Math.PI) + 90;
+    
+        return `rotate(${deg}deg)`;
+    })
+    
+</script>
+
+<style scoped>
+    #rotateMe {
+        transform-origin: center center;
+        transform: v-bind(cssRotate);
+        display: inline-block;
+    }
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,16 @@
+﻿<template>
+    <slot name="top"></slot>
+    
+    <button @click="$event => counter++">{{counter}}</button>
+    
+    <slot name="bottom"></slot>
+</template>
+
+<script setup lang="ts">
+    import {ref} from 'vue'
+    
+    const props = defineProps({
+        initialCount: Number,
+    })
+    const counter = ref<number>(props.initialCount ?? 0)
+</script>
